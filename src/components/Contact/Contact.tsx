3a1f8941c61a87e318c7dc1b9763876b0b95de89
YanivWein24 <yanivwein22@gmail.com>
2022-07-12 15:52:26 +0300

    Added styling to 'Contact' section & Custom Scrollbar

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,54 +5,50 @@ import "./Contact.css"
 
 const Contact: FC = () => {
 
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("")
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
 
     const sendEmail = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log(firstName, lastName, email, message)
 
         const publicKey: string = process.env.REACT_APP_PUBLIC_KEY ? process.env.REACT_APP_PUBLIC_KEY : "error"
         const templateId: string = process.env.REACT_APP_TEMPLATE_ID ? process.env.REACT_APP_TEMPLATE_ID : "error"
         const serviceId: string = process.env.REACT_APP_SERVICE_ID ? process.env.REACT_APP_SERVICE_ID : "error"
 
+        const resetForm = (): void => {
+            setName("")
+            setEmail("")
+            setMessage("")
+        }
+
         emailjs.sendForm(serviceId, templateId, e.currentTarget, publicKey)
             .then((result) => {
                 console.log(result.text)
-                // e.currentTarget.reset()
-                setFirstName("")
-                setLastName("")
-                setEmail("")
-                setMessage("")
+                resetForm()
             }, (error) => {
                 console.log(error.text)
             })
     }
 
-
     return (
-        <Container id="Contact">
+        <div id="Contact">
             <h1>contact me:</h1>
-            <form onSubmit={sendEmail}>
-                <Row>
-                    <Col>
-                        <input type="text" className="name firstName" value={firstName} placeholder="First Name" name="firstName" onChange={(e) => setFirstName(e.target.value)}></input>
-                    </Col >
-                    <Col>
-                        <input type="text" className="name lastName" value={lastName} placeholder="Last Name" name="lastName" onChange={(e) => setLastName(e.target.value)}></input>
-                    </Col>
-                </Row>
-                <Row>
-                    <input type="email" className="email" value={email} placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)}></input>
-                </Row>
-                <Row>
-                    <textarea placeholder="Your Message Here" value={message} name="message" onChange={(e) => setMessage(e.target.value)}></textarea>
-                </Row>
-                <Button type="submit">Send</Button>
-            </form>
-        </Container>
+            <Container>
+                <form onSubmit={sendEmail}>
+                    <div className="fullName">
+                        <input type="text" value={name} required={true} placeholder="Name" name="name" onChange={(e) => setName(e.target.value)}></input>
+                    </div>
+                    <div className="email">
+                        <input type="email" value={email} required={true} placeholder="Email" name="email" onChange={(e) => setEmail(e.target.value)}></input>
+                    </div>
+                    <div className="message">
+                        <textarea rows={5} placeholder="Your Message Here" value={message} required={true} name="message" onChange={(e) => setMessage(e.target.value)}></textarea>
+                    </div>
+                    <button type="submit">Send <i className="fa-solid fa-paper-plane"></i></button>
+                </form>
+            </Container>
+        </div>
     )
 }
 
