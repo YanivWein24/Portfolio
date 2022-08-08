1715b69957618918ada07f2dc35378cfb2918f09
YanivWein24 <yanivwein22@gmail.com>
2022-08-08 16:38:17 +0300

    Added SVGs to style the 'About' and 'Projects' components

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -36,7 +36,12 @@ const About: FC = () => {
                 <Button onMouseOver={handleSecondMouseOver} onMouseOut={handleSecondMouseOut} className={secondButtonHovering ? "shake-bottom" : ""}>
                     <Link to="Contact" smooth={true} duration={1000} offset={-60} className="link"><i className="fa-solid fa-envelope"></i> &nbsp;Contact Me</Link></Button >
             </div >
-            <Row lg={3} md={12} sm={12} className="about-me">
+            <div className="wave">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+                </svg>
+            </div>
+            <Row lg={3} md={12} sm={12} className="about-me no-gutters" >
                 <Col md={12} data-aos={smallScreen ? "fade-left" : "zoom-in-down"} data-aos-once="true" className="solving-problems">
                     <i className="fa-solid fa-laptop-code"></i>
                     <h3>Solving Problems</h3>
@@ -61,6 +66,11 @@ const About: FC = () => {
                     </p>
                 </Col>
             </Row>
+            <div className="bottom-wave">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+                </svg>
+            </div>
         </div >
     )
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,17 +5,28 @@ import myProjects from "./myProjects"
 import "./Projects.css"
 
 const Projects: FC = () =>
-    <div data-aos="slide-up" data-aos-once="true" className="projects" id="Projects">
-        <h1>Projects:</h1>
-        <Container>
-            <Row>
-                {myProjects.map(project => (
-                    <Col data-aos="slide-up" data-aos-once="true" key={project.id} sm={12} md={project.mediumSize ? 12 : 6} lg={3}>
-                        <Cards project={project} />
-                    </Col>
-                ))}
-            </Row>
-        </Container>
+    <div style={{ position: "relative" }} data-aos="slide-up">
+        <div className="topAngle-svg">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
+            </svg>
+        </div>
+        <div data-aos-once="true" className="projects" id="Projects">
+            <h1>Projects:</h1>
+            <Container>
+                <Row className='no-gutters'>
+                    {myProjects.map(project => (
+                        <Col data-aos="slide-up" data-aos-once="true" key={project.id} sm={12} md={project.mediumSize ? 12 : 6} lg={3}>
+                            <Cards project={project} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </div>
+        <div className="bottomAngle-svg">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z" className="shape-fill"></path>
+            </svg>
+        </div>
     </div>
-
 export default Projects
