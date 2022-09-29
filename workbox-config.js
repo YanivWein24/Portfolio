5859c5c9f922c40281e4bf200ce0d0fd97569538
Your Name <yanivwein22@gmail.com>
2022-09-29 14:11:31 +0300

    Added workbox-config.js in order to include the large mp4 file in cache storage

---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	globDirectory: 'src/',
+	globPatterns: [
+		'**/*.{tsx,mp4,css,ts,pdf}',
+	],
+	swDest: 'src/service-worker.ts',
+	swSrc: 'src/service-worker.ts',
+	maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
+};
