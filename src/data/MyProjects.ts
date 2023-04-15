69a6fba2815574bc82e59b606091f9980cdf5a90
YanivWein24 <yanivwein22@gmail.com>
2023-04-15 23:45:58 +0300

    update projects section, fix header responsive issues on some medium devices

--- src/data/MyProjects.ts
+++ src/data/MyProjects.ts
@@ -44,7 +44,7 @@ const MyProjects: Project[] = [
     id: 0,
     name: "Get Jokes 😂",
     description:
-      "A MERN stack application that provides custom jokes from Joke-API. Users can register (also using their Google / Meta accounts) and create their private collection of jokes.",
+      "Full-Stack application that provides custom jokes from Joke-API. Users can register (also using their Google / Meta accounts) and create their private collection of jokes.",
     image: [
       "https://user-images.githubusercontent.com/97472180/211169796-f8472f4d-5cac-46bb-b156-8d071211a9cf.PNG",
       "https://user-images.githubusercontent.com/97472180/211169602-9b7471de-dd74-4d04-adf8-2bc5ba27a4f3.gif",
@@ -67,31 +67,31 @@ const MyProjects: Project[] = [
   },
   {
     id: 1,
-    name: "Chat App 💬",
+    name: "Post It 🔥",
     description:
-      "Cross-platform chat Application. Users can join or create live rooms, and chat with their friends online! It supports all the classic emojis, auto notifies when users join/leave the room, and contain a dedicated screen that presents the current users in the room.",
+      "Full-Stack social media application built with Next.js. Users can sign up and authenticate with Google or GitHub to start sharing and commenting on posts.",
     image: [
-      "https://user-images.githubusercontent.com/97472180/178511493-91d1e17a-3203-4f45-b74c-eb87b28b019b.PNG",
-      "https://user-images.githubusercontent.com/97472180/191557958-d28c9db2-dc75-4e8b-a4f9-bf31f71633b3.gif",
+      "https://user-images.githubusercontent.com/97472180/230383665-d73d4364-c24a-4ecd-b2d8-a5ecfdda23ef.png",
+      "https://user-images.githubusercontent.com/97472180/230383676-ba8c0517-d6ea-4419-b3d3-8c762c5a5ae9.gif",
     ],
-    tools: ["React", "Router", "Node.js", "Express", "Socket-IO"],
+    tools: ["Next.js", "TypeScript", "Next-auth", "Prisma", "PostgreSQL", "React-Query", "TailwindCSS"],
     links: [
-      "https://github.com/YanivWein24/Chat-App-Socket-IO",
-      "https://chat-app-faoy.onrender.com/",
+      "https://github.com/YanivWein24/Post-It-Next.js",
+      "https://post-it-next-js.vercel.app/",
     ],
   },
   {
     id: 2,
     name: "IP Tracker 🌎",
     description:
-      "A tracking app that locates users by their IP address. Can locate any valid IPv4 address on the globe! Using 3 different APIs: to find the local IP (optional), track its location, and display it on a live map!",
+      "Tracking app that locates users by their IP address. Can locate any valid IPv4 address on the globe! Using 3 different APIs: to find the local IP (optional), track its location, and display it on a live map!",
     image: [
       "https://user-images.githubusercontent.com/97472180/196441374-09abfd78-c2d8-4144-8d4a-1e0bca7322fa.PNG",
       "https://user-images.githubusercontent.com/97472180/196443648-30870f0c-fe51-4971-949a-6458f873f54f.gif",
     ],
     tools: [
       "React",
-      "Typescript",
+      "TypeScript",
       "IPify-API",
       "IP-Geolocation-API",
       "Leaflet-Map",
@@ -109,7 +109,7 @@ const MyProjects: Project[] = [
       "https://user-images.githubusercontent.com/97472180/205455630-d3a19391-159b-4010-9a39-9c6750c81cc8.png",
       "https://user-images.githubusercontent.com/97472180/205455533-faa3ca83-3043-4bf3-9de9-9ee08acef7ec.gif",
     ],
-    tools: ["React", "Typescript", "SASS", "shrtco.de API"],
+    tools: ["React", "TypeScript", "SCSS", "shrtco.de API"],
     links: [
       "https://github.com/YanivWein24/shortly",
       "https://shortly-links-shortener.netlify.app/",
@@ -124,7 +124,7 @@ const MyProjects: Project[] = [
       "https://user-images.githubusercontent.com/97472180/196447088-96140c51-4b8f-47bc-b5d2-8413c248838a.PNG",
       "https://user-images.githubusercontent.com/97472180/195998154-d0aa6a65-1035-48e1-89f4-e9707b1cea64.gif",
     ],
-    tools: ["React", "Typescript", "Context", "GitHub-API"],
+    tools: ["React", "TypeScript", "Context", "GitHub-API"],
     links: [
       "https://github.com/YanivWein24/Git-Finder",
       "https://git-finderr.netlify.app/",
@@ -139,7 +139,7 @@ const MyProjects: Project[] = [
       "https://user-images.githubusercontent.com/97472180/194753848-154c5853-c190-4201-bae2-4560e97c0d84.PNG",
       "https://user-images.githubusercontent.com/97472180/194753796-148c3e8a-6fe4-450a-9777-760785a85a3f.gif",
     ],
-    tools: ["React", "Typescript", "Bootstrap", "AOS", "EmailJS"],
+    tools: ["React", "TypeScript", "Bootstrap", "AOS", "EmailJS"],
     links: ["https://github.com/YanivWein24/Portfolio", "/"],
   },
 ];
